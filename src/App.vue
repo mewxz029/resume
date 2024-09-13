b8bd62da974425fcd4916ec742c1ebff904b050c
mewxz029 <mewlawbreakertv@gmail.com>
2024-09-14 05:39:56 +0700

    add content

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,65 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="px-4 py-12 w-svw bg-gray-900 text-white text-start md:text-center h-full lg:h-svh">
+    <h1 class="font-semibold text-3xl">Wuttinan Sukpoon</h1>
+    <p>
+      <a href="mailto: wuttinan.sukpoon@gmail.com">wuttinan.sukpoon@gmail.com</a>
+    </p>
+    <p>
+      <a href="https://github.com/mewxz029?" target="_blank">@mewxz029</a>
+    </p>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it! Github Pages" />
-    </div>
-  </header>
+    <p class="font-semibold mt-10">Mid-Level | Full-Stack Developer</p>
+    <p class="font-thin italic text-sm">
+      Thailand-based with 3 years of experience in Software Development.
+    </p>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <p class="mt-10">
+      As Team Lead at
+      <a
+        href="https://www.softnova.co"
+        target="_blank"
+        class="font-semibold text-[#9353A3] underline-offset-2 underline"
+        >Softnova</a
+      >
+    </p>
+    <p class="mt-10 font-light">
+      I’m a mid-level Full-stack Developer focusing on backend development with
+    </p>
+    <p class="">
+      <span class="text-blue-500 font-bold">TypeScript </span> and
+      <span class="text-red-500 font-bold">NestJS</span>.
+    </p>
+    <p class="">I also handle frontend projects using</p>
+    <p class="">
+      <span class="text-green-300 font-bold">Vue</span> and
+      <span class="text-green-400 font-bold">Nuxt</span>.
+    </p>
+
+    <p class="mt-10">I’m proficient in both SQL (PostgreSQL, MySQL, MariaDB) and NoSQL (MongoDB)</p>
+    <p class="">along with Redis for caching.</p>
+    <p class="mt-10">I have hands-on experience with message queues and WebSockets.</p>
+    <p>
+      I mostly use <span class="font-semibold text-blue-300">Docker 🐳</span> for development, set
+      up CI/CD pipelines on GitLab/GitHub, and deploy on GCP and AWS.
+    </p>
+
+    <p class="">
+      Currently, I’m studying ElysiaJS and expanding my skills with
+      <span class="font-semibold text-blue-400">Golang</span>,
+      <span class="font-semibold text-orange-700">Rust</span>, and
+      <span class="font-semibold text-red-400">Ruby on Rails</span>.
+    </p>
+
+    <p>
+      By the way, I use <span class="font-bold">Neovim</span> as my primary editor, check my repo
+      <a
+        href="https://github.com/mewxz029/dotfiles"
+        target="_blank"
+        class="font-semibold text-blue-300 underline-offset-2 underline"
+        >dotfiles</a
+      >
+    </p>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
